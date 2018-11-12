3f5c6a42a98d652a37576b787169647065c5b080
Neha Sadhvi <40807157+scorpvi@users.noreply.github.com>
2018-11-11 16:38:58 -0800

    Added functionalities to fetch events from API

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -3,6 +3,13 @@ class EventBright {
         this.auth_token = 'XMMF622M2NCHD72Z6RD7';
     }
 
+    //Get the list of all the events based on user input
+    async getEvents(eventName, categoryName) {
+        const eventUrl = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&categories=${categoryName}&token=${this.auth_token}`);
+        const events = await eventUrl.json();
+        return {events};
+    }
+
     //Get the category list from Event Bright API
     async getCategoryList() {
         //Query the API
