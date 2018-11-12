d89bb4f98a545e604d51b64be37c7190df2fab14
Neha Sadhvi <40807157+scorpvi@users.noreply.github.com>
2018-11-11 16:38:23 -0800

    Added functionalities to display events

--- js/ui.js
+++ js/ui.js
@@ -5,6 +5,35 @@ class UI {
 
     init(){
         this.printCategories();
+        this.result = document.getElementById('result');
+    }
+
+    displayEvents(events) {
+        let HTMLTemplate = '';
+
+        events.forEach(eventInfo => {
+            HTMLTemplate = HTMLTemplate += `
+            <div class="col-md-4 mt-4">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="img-fluid mb-2" src="${eventInfo.logo !== null ? eventInfo.logo.url : ''}"> 
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                                <h2 class="text-center card-title">${eventInfo.name.text}</h2>
+                                <p class="lead text-info">Event Information:</p>
+                                <p>${eventInfo.description.text.substring(0,200)}...</p>
+                                <span class="badge badge-primary">Capacity: ${eventInfo.capacity}</span>
+                                <span class="badge badge-secondary">Date & Time: ${eventInfo.start.local}</span>
+
+                                <a href="${eventInfo.url}" target="_blank" class="btn btn-primary btn-block mt-4">Get Tickets</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        });
+        this.result.innerHTML = HTMLTemplate;
     }
 
     printCategories() {
@@ -22,5 +51,30 @@ class UI {
                 });
             })
             .catch(err => console.log(err));
+    }
+
+    printMessage(message, className) {
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+
+        const prevResult = document.querySelector('#result div'); 
+
+        if(prevResult) {
+            this.result.innerHTML = "";
+        }
+
+        document.getElementById('search-events').appendChild(div);
+
+        setTimeout(() => {
+            this.removeMessage();
+        }, 2000);
+    }
+
+    removeMessage() {
+        const alert = document.querySelector('.alert');
+        if(alert) {
+            alert.remove();
+        }
     }
 }
